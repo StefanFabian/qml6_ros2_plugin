51f7bfbb17949c397f4d79015e89cd08d2e7167d
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-12-20 16:32:39 +0100

    Fixed crash when ServiceClient is processing request while client is destroyed. Use QPointer for callbacks to prevent crashes due to the object being destroyed while an asynchronous operation is still in progress.

--- src/action_client.cpp
+++ src/action_client.cpp
@@ -116,21 +116,20 @@ QObject *ActionClient::sendGoalAsync( const QVariantMap &goal, QJSValue options 
     return nullptr;
   }
 
-  std::string goal_type = action_type_.toStdString();
-  goal_type = goal_type.substr( 0, goal_type.length() - strlen( "Action" ) ) + "Goal";
   try {
     auto message = client_->create_goal();
     if ( !fillMessage( message, goal ) )
       return nullptr;
+    QPointer instance = this;
     BabelFishActionClient::SendGoalOptions goal_options;
     if ( options.hasProperty( "onGoalResponse" ) ) {
       goal_options.goal_response_callback =
           [goal_response_cb = options.property( "onGoalResponse" ),
-           this]( const BabelFishActionClient::GoalHandle::SharedPtr &gh ) {
-            if ( !goal_response_cb.isCallable() )
+           instance]( const BabelFishActionClient::GoalHandle::SharedPtr &gh ) {
+            if ( !instance || !goal_response_cb.isCallable() )
               return;
             QMetaObject::invokeMethod(
-                this, "invokeGoalResponseCallback", Qt::AutoConnection,
+                instance, "invokeGoalResponseCallback", Qt::AutoConnection,
                 Q_ARG( QJSValue, goal_response_cb ),
                 Q_ARG( ros_babel_fish::BabelFishActionClient::GoalHandle::SharedPtr, gh ) );
           };
@@ -138,12 +137,13 @@ QObject *ActionClient::sendGoalAsync( const QVariantMap &goal, QJSValue options 
     if ( options.hasProperty( "onFeedback" ) ) {
       goal_options.feedback_callback =
           [feedback_cb = options.property( "onFeedback" ),
-           this]( const BabelFishActionClient::GoalHandle::SharedPtr &goal_handle,
-                  const CompoundMessage::ConstSharedPtr &feedback ) mutable {
-            if ( !feedback_cb.isCallable() )
+           instance]( const BabelFishActionClient::GoalHandle::SharedPtr &goal_handle,
+                      const CompoundMessage::ConstSharedPtr &feedback ) mutable {
+            if ( !instance || !feedback_cb.isCallable() )
               return;
             QMetaObject::invokeMethod(
-                this, "invokeFeedbackCallback", Qt::AutoConnection, Q_ARG( QJSValue, feedback_cb ),
+                instance, "invokeFeedbackCallback", Qt::AutoConnection,
+                Q_ARG( QJSValue, feedback_cb ),
                 Q_ARG( ros_babel_fish::BabelFishActionClient::GoalHandle::SharedPtr, goal_handle ),
                 Q_ARG( ros_babel_fish::CompoundMessage::ConstSharedPtr, feedback ) );
           };
@@ -151,11 +151,11 @@ QObject *ActionClient::sendGoalAsync( const QVariantMap &goal, QJSValue options 
     if ( options.hasProperty( "onResult" ) ) {
       goal_options.result_callback =
           [result_cb = options.property( "onResult" ),
-           this]( const BabelFishActionClient::GoalHandle::WrappedResult &result ) {
-            if ( !result_cb.isCallable() )
+           instance]( const BabelFishActionClient::GoalHandle::WrappedResult &result ) {
+            if ( !instance || !result_cb.isCallable() )
               return;
             QMetaObject::invokeMethod(
-                this, "invokeResultCallback", Qt::AutoConnection, Q_ARG( QJSValue, result_cb ),
+                instance, "invokeResultCallback", Qt::AutoConnection, Q_ARG( QJSValue, result_cb ),
                 Q_ARG( QString, uuidToString( result.goal_id ) ),
                 Q_ARG( qml6_ros2_plugin::action_goal_status::GoalStatus,
                        static_cast<qml6_ros2_plugin::action_goal_status::GoalStatus>( result.code ) ),
